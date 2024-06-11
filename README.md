e353f9f928e4f54cc96422d6f86effd33c841e1c
versadev <support@versadev.com>
2024-06-11 15:39:22 +0930

    Completed course

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 This is the starter template for the Next.js App Router Course. It contains the starting code for the dashboard application.
 
 For more information, see the [course curriculum](https://nextjs.org/learn) on the Next.js Website.
+
+WB: This example was created from the following course: https://nextjs.org/learn/dashboard-app
+
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,15 +1,42 @@
 'use server';
 
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 import { z } from 'zod'; // TypeScript-first validation library
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
+
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+      invalid_type_error: 'Please select a customer.',
+    }),
+    amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
+    status: z.enum(['pending', 'paid'], {
+      invalid_type_error: 'Please select an invoice status.',
+    }),
     date: z.string(),
   });
 
@@ -17,7 +44,17 @@ const FormSchema = z.object({
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
+// This is temporary until @types/react-dom is updated
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
+export async function createInvoice(prevState: State, formData: FormData) {
     
     /*
     const rawFormData = {
@@ -32,20 +69,49 @@ export async function createInvoice(formData: FormData) {
 
     /* See also const rawFormData = Object.fromEntries(formData.entries()) Chapter 12 - Mutating Data */
 
+    /*
     const { customerId, amount, status } = CreateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
+    */
+
+    // Validate form fields using Zod
+    const validatedFields = CreateInvoice.safeParse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+
+    // If form validation fails, return errors early. Otherwise, continue.
+    if (!validatedFields.success) {
+      return {
+        errors: validatedFields.error.flatten().fieldErrors,
+        message: 'Missing Fields. Failed to Create Invoice.',
+      };
+    }
+
+    // Prepare data for insertion into the database
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100; // Good practice to store monetary values in cents in your database to eliminate JavaScript floating-point errors.
     const date = new Date().toISOString().split('T')[0]; // Create a new date with the format "YYYY-MM-DD" for the invoice's creation date.
     //console.log(date.toString()); // Since server-side, logs to VSCode terminal
 
-    await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
+    // Insert data into the database
+    try {
+      await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+      `;
+    } catch (error) {
+      // If a database error occurs, return a more specific error
+      return {
+        message: 'Database Error: Failed to Create Invoice.',
+      };
+    }
 
+    // Revalidate the cache for the invoices page and redirect the user.
     // Since we are updating the data displayed in the invoices route, we must clear the cache and trigger a new request to the server.
     // The changes to the database will then be displayed: otherwise what is displayed comes from the cache.
     revalidatePath('/dashboard/invoices');
@@ -63,11 +129,15 @@ export async function updateInvoice(id: string, formData: FormData) {
    
     const amountInCents = amount * 100; // Good practice to store monetary values in cents in your database to eliminate JavaScript floating-point errors.
    
-    await sql`
-      UPDATE invoices
-      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-      WHERE id = ${id}
-    `;
+    try {
+      await sql`
+          UPDATE invoices
+          SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+          WHERE id = ${id}
+        `;
+    } catch (error) {
+      return { message: 'Database Error: Failed to Update Invoice.' };
+    }
 
     // Since we are updating the data displayed in the invoices route, we must clear the cache and trigger a new request to the server.
     // The changes to the database will then be displayed: otherwise what is displayed comes from the cache.
@@ -79,8 +149,15 @@ export async function updateInvoice(id: string, formData: FormData) {
 
 export async function deleteInvoice(id: string) {
 
+    //throw new Error('Failed to Delete Invoice');
+
     // Since this action is being called in the /dashboard/invoices path, you don't need to call redirect.
     // Calling revalidatePath will trigger a new server request and re-render the table.
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
-    revalidatePath('/dashboard/invoices');
+    try {
+      await sql`DELETE FROM invoices WHERE id = ${id}`;
+      revalidatePath('/dashboard/invoices');
+      return { message: 'Deleted Invoice.' };
+    } catch (error) {
+      return { message: 'Database Error: Failed to Delete Invoice.' };
+    }
 }
